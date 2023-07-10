e2fc2972ddad121dca2bcae10c2cc066158d9888
Jiin <87533661+ljilji@users.noreply.github.com>
2023-07-10 22:58:37 +0900

    Update server.py

--- server.py
+++ server.py
@@ -1,54 +1,74 @@
 import socket
-import json
 IP = socket.gethostbyname(socket.gethostname())
 PORT = 4455
 ADDR = (IP, PORT)
 SIZE = 1024
 FORMAT = "utf-8"
 
+
 def main():
-    
+    connecting()
+
+
+def connecting():
     print("[STARTING] Server is starting.")
-   
+    
+    """ Staring a TCP socket. """
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
- 
+    print(ADDR)
+    """ Bind the IP and PORT to the server. """
     server.bind(ADDR)
+    print(IP)
     
-    
+    """ Server is listening, i.e., server is now waiting for the client to connected. """
     server.listen()
     print("[LISTENING] Server is listening.")
-    
+    global conn
+ 
+
     
     while True:
-        
+        """ Server has accepted the connection from the client. """
         conn, addr = server.accept()
         print(f"[NEW CONNECTION] {addr} connected.")
+        while True:
+            if receiving() is False:
+                break
 
 
-        """ready to open json"""
-        
 
-
-        
-        filename = conn.recv(SIZE).decode(FORMAT)
-        print(f"[RECV] Receiving the filename.")
-        file = open(filename, "w")
-        conn.send(("Filename received : %s"%filename).encode(FORMAT))
-    
-    
-      
-        data = conn.recv(SIZE).decode(FORMAT)
-        print(f"[RECV] Receiving the file data.")
-        file.write(data)
-        conn.send(("File data received : %s"%data).encode(FORMAT))
-        
-    
-        file.close()
-    
-    
+        """ Closing the connection from the client. """
         conn.close()
         print(f"[DISCONNECTED] {addr} disconnected.")
 
-if __name__ == "__main__":
-        main()
+
+def receiving():
+    """ Receiving the filename, filedata from the client. """
+    data = conn.recv(4)
+    len = int.from_bytes(data, "little")
+    if len == 0:
+        return False
+
+    data = conn.recv(len)
+    recv_data = data.decode(FORMAT)
+    separating(recv_data) 
+      
+    
+
+def separating(recv_data):
+    data = recv_data.split(sep=';')
+
+    print(data)
+
+    if data[1] == "END_OF_FILE":
+        return True
+    logging(data[0], data[1])
+
+
+def logging(file_name, file_data):
+    fd = open(file_name + ".txt", 'a+')
+    file_name = fd.writelines(file_data)
+    fd.close() 
+
+if __name__ == '__main__':
+    main()
